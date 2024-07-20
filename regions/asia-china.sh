371a786766857c422526d3d409702dfb9d08b97e
Emux <devemux86@gmail.com>
2024-07-20 11:04:14 +0300

    Add China regions (#38)

---
+++ regions/asia-china.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+../map-creator.sh asia/china/anhui ram en,de,fr,es
+../map-creator.sh asia/china/beijing ram en,de,fr,es
+../map-creator.sh asia/china/chongqing ram en,de,fr,es
+../map-creator.sh asia/china/fujian ram en,de,fr,es
+../map-creator.sh asia/china/gansu ram en,de,fr,es
+../map-creator.sh asia/china/guangdong ram en,de,fr,es
+../map-creator.sh asia/china/guangxi ram en,de,fr,es
+../map-creator.sh asia/china/guizhou ram en,de,fr,es
+../map-creator.sh asia/china/hainan ram en,de,fr,es
+../map-creator.sh asia/china/hebei ram en,de,fr,es
+../map-creator.sh asia/china/heilongjiang ram en,de,fr,es
+../map-creator.sh asia/china/henan ram en,de,fr,es
+../map-creator.sh asia/china/hong-kong ram en,de,fr,es
+../map-creator.sh asia/china/hubei ram en,de,fr,es
+../map-creator.sh asia/china/hunan ram en,de,fr,es
+../map-creator.sh asia/china/inner-mongolia ram en,de,fr,es
+../map-creator.sh asia/china/jiangsu ram en,de,fr,es
+../map-creator.sh asia/china/jiangxi ram en,de,fr,es
+../map-creator.sh asia/china/jilin ram en,de,fr,es
+../map-creator.sh asia/china/liaoning ram en,de,fr,es
+../map-creator.sh asia/china/macau ram en,de,fr,es
+../map-creator.sh asia/china/ningxia ram en,de,fr,es
+../map-creator.sh asia/china/qinghai ram en,de,fr,es
+../map-creator.sh asia/china/shaanxi ram en,de,fr,es
+../map-creator.sh asia/china/shandong ram en,de,fr,es
+../map-creator.sh asia/china/shanghai ram en,de,fr,es
+../map-creator.sh asia/china/shanxi ram en,de,fr,es
+../map-creator.sh asia/china/sichuan ram en,de,fr,es
+../map-creator.sh asia/china/tianjin ram en,de,fr,es
+../map-creator.sh asia/china/tibet ram en,de,fr,es
+../map-creator.sh asia/china/xinjiang ram en,de,fr,es
+../map-creator.sh asia/china/yunnan ram en,de,fr,es
+../map-creator.sh asia/china/zhejiang ram en,de,fr,es
